9a7f36c2d543bef00394014d4ecf5de46d550618
UO276900 <adrian.estrada2001@gmail.com>
2022-03-13 20:35:41 +0100

    Prueba pantalla bienvenida

--- webapp/src/components/Welcome.tsx
+++ webapp/src/components/Welcome.tsx
@@ -2,6 +2,7 @@ import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import logo from '../logoRock.png';
 import rockImg from '../rocksWelcome.jpg';
+import { Button } from '@mui/material';
 
 type WelcomeProps = {
   message: string;
@@ -13,8 +14,10 @@ function Welcome(props: WelcomeProps): JSX.Element {
     <Grid container>
       <Grid item xs={12}>
         <Box component="h2">Bienvenidos a {props.message}</Box>
+        <Button variant="contained" type="submit" sx={{ my: 2 }}>Iniciar Sesion</Button>
+        <Button variant="contained" type="submit" sx={{ my: 2 }}>Registrate</Button>
       </Grid>
-      <Grid item xs={12}>
+      <Grid item xs={20}>
         <img src={logo} className="App-logo" alt="logo" />
       </Grid>
       
