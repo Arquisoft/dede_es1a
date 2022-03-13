47fa044895b26bef6dbf18845cbd9ff134700393
Ignacio Gómez Gasch <75808025+UO271548@users.noreply.github.com>
2022-03-13 19:41:30 +0100

    Separación api.ts y controllers

--- restapi/api.ts
+++ restapi/api.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response, Router } from 'express';
 import {check} from 'express-validator';
+import {findRocks, addRock, deleteRock} from './controllers/RockController';
+import {findUsers, addUser, deleteUser} from './controllers/UserController';
 const User = require("./models/User");
 const Rock = require("./models/Rock");
 
@@ -7,87 +9,17 @@ const api:Router = express.Router()
 const mongoose = require("mongoose");
 
 //Methods for control users from the app
-api.get("/users/list", async (req, res) => {
-  const users = await User.find({})
-  res.send(users);
-});
+api.get("/users/list", findUsers);
 
-api.post("/users/add", async (req, res) => {
+api.post("/users/add", addUser);
 
-  let name = req.body.name;
-  let email = req.body.email;
-
-  let user = await User.findOne(
-      { email: email }
-  );
-  if (user) {
-      res.send({ error: "Error: This user is already registered " + email });
-  }
-  else {
-      user = new User({
-          name: name,
-          email: email
-      });
-      await user.save();
-      res.send(user);
-  }
-});
-
-api.post("/users/delete", async (req, res) => {
-
-  let email = req.body.email;
-
-  let user = await User.deleteOne(
-      { email: email }
-  );
-  res.send(user);
-});
+api.post("/users/delete", deleteUser);
 
 //Methods for product of the app
-api.get("/rocks/list", async (req, res) => {
-  const rocks = await Rock.find({})
-  res.send(rocks);
-});
+api.get("/rocks/list", findRocks);
 
-api.post("/rocks/add", async (req, res) => {
+api.post("/rocks/add", addRock);
 
-  let name = req.body.name;
-  let type = req.body.type;
-  let description = req.body.description;
-  let price = req.body.price;
-  let mohsHardness = req.body.mohsHardness;
-  let density = req.body.density;
-  let img = req.body.img;
-
-  let stone = await Rock.findOne(
-      { name: name }
-  );
-  if (stone) {
-      res.send({ error: "Error: This stone is already in the app " + name });
-  }
-  else {
-    stone = new Rock({
-          name: name,
-          type: type,
-          description: description,
-          price: price,
-          mooseHardness: mohsHardness,
-          density: density,
-          img: img
-      });
-      await stone.save();
-      res.send(stone);
-  }
-});
-
-api.post("/rocks/delete", async (req, res) => {
-
-  let rockId = req.body.rockId;
-
-  let rock = await Rock.deleteOne(
-      { _id: mongoose.Types.ObjectId(rockId) }
-  );
-  res.send(rock);
-});
+api.post("/rocks/delete", deleteRock);
 
 export default api;
