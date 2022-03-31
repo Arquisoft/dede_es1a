1b785520a49ce17c223439a5adcab133a66bcc9b
Ignacio Gómez Gasch <75808025+UO271548@users.noreply.github.com>
2022-03-31 15:17:09 +0200

    Calculo gastos de envio/operaciones base compras

--- restapi/controllers/OrderController.ts
+++ restapi/controllers/OrderController.ts
@@ -1,5 +1,13 @@
 import express, { Request, Response, Router } from 'express';
+const NodeGeocoder = require('node-geocoder');
+
+const options = {
+  provider: 'openstreetmap',
+};
+
+const geocoder = NodeGeocoder(options);
 const Order = require("../models/Order");
+
 
 export const findOrdersByUserDni = async (req:Request, res:Response) => {
     let dni = req.body.dni;
@@ -31,3 +39,41 @@ export const addOrder = async (req:Request, res:Response): Promise<any> => {
     res.send(order);
     
   };
+
+
+  export const getDeliveryCosts = async (req:Request, res:Response) : Promise<any> =>{ 
+    
+    let address = req.body.address;
+    let deliveryCosts = 0;
+    const addressCordinates = await geocoder.geocode(address);
+
+    let string = JSON.stringify(addressCordinates);
+    let objectValue = JSON.parse(string);
+
+    let latitudeAddress = objectValue[0].latitude
+    let longitudeAddress = objectValue[0].longitude
+
+    let latitudeOrigin = 43.364029479589455;
+    let longitudeOrigin = -5.851265656091869;
+
+    let R = 6371;
+    let distanceLatitude = toRadians(latitudeAddress-latitudeOrigin);
+    let distanceLongitude = toRadians(longitudeAddress-longitudeOrigin);
+    let lat1 = toRadians(latitudeOrigin);
+    let lat2 = toRadians(latitudeAddress);
+
+    let a = Math.sin(distanceLatitude/2) * Math.sin(distanceLatitude/2) +
+        Math.sin(distanceLongitude/2) * Math.sin(distanceLongitude/2) * Math.cos(lat1) * Math.cos(lat2); 
+
+    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    let distance = R * c;
+
+    deliveryCosts = Number.parseFloat((distance * 0.5).toFixed(2));
+
+    res.send(deliveryCosts.toString());
+  }
+
+  function toRadians(value: number) 
+    {
+        return value * Math.PI / 180;
+    }
