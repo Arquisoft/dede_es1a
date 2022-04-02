985b0111561f56ba25df68e6ba7f1d94849cef7b
Toraño <UO276853@uniovi.es>
2022-04-02 21:15:41 +0200

    make it better

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useQuery } from 'react-query';
 import Box from '@mui/material/Box';
 //import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
@@ -8,7 +9,7 @@ import logo from './images/interfaz/logoRock.png';
 import Title from './components/titleUtil';
 import  {getUsers, getRocas} from './api/api';
 
-import { createTheme } from '@mui/material';
+import { createTheme, List } from '@mui/material';
 import './css/App.css';
 import ResponsiveAppBar from './components/navBar';
 
@@ -17,12 +18,55 @@ import Catalogo from './components/Catalogo';
 import { ThemeProvider } from '@emotion/react';
 //import {createData} from "./code/insertExampleData"
 
+import Drawer from '@material-ui/core/Drawer';
+import ShoppingCart from './shoppingCart/ShoppingCart';
+import { Wrapper, StyledButton } from './App.styles';
+import Badge from '@material-ui/core/Badge';
+
+
 function App(): JSX.Element {
   const [rocas,setRocas] = useState<Roca[]>([]);
 
   const refreshRocaList = async () => {
     setRocas(await getRocas());
   }
+
+  // Shopping cart
+  const [isCartOpen, setCartOpen] = useState(true);
+
+  const[cartItems, setCartItems] = useState(new Map<String, number>());
+
+  const getNofItemsCart = (cartItems: Map<String, number> ) => {
+    let n = 0;
+    for(let key in cartItems.keys()) {
+      n+= cartItems.get(key) as number;
+    }
+  }
+
+  const handleAddToCart = (selectedItem: Roca) => {
+    setCartItems(cart => {
+      let quantity = cart.has(selectedItem.name) ? cart.get(selectedItem.name) as number : 0;
+
+      cart.set(selectedItem.name, quantity+1);
+      return cart;
+    })
+  };
+
+  const handleRemoveFromCart = (selectedItem: Roca) => {
+    setCartItems( cart=> {
+      let quantity = cart.has(selectedItem.name) ? cart.get(selectedItem.name) as number : 0;
+
+      if(quantity == 1) {
+        cart.delete(selectedItem.name);
+      } else {
+        cart.set(selectedItem.name, quantity-1);
+      }
+
+      return cart;
+    });
+
+  };
+
 
   useEffect(()=>{
     refreshRocaList();
@@ -47,10 +91,27 @@ function App(): JSX.Element {
   });
   
   return (
+
+
     <ThemeProvider theme={theme}>
       <ResponsiveAppBar/>
+      
+      <Drawer anchor='right' open={isCartOpen} onClose={() => setCartOpen(false)}>
+          <ShoppingCart
+            rocas = {rocas}
+            cartContent={cartItems}
+            handleAddToCart={handleAddToCart}
+            handleRemoveFromCart={handleRemoveFromCart}
+          />
+      </Drawer>
+
+      <StyledButton onClick={() => setCartOpen(true)}>
+        
+      </StyledButton>
+      
+
       <Welcome message="ASW students"/>
-      <Catalogo rocas={rocas}/>
+      <Catalogo rocas={rocas} handleAddToCart={handleAddToCart}/>
     </ThemeProvider>
   );
 }
---
+++ webapp/src/components/ShoppingCartComponent.tsx
@@ -0,0 +1,49 @@
+import {Roca} from '../shared/shareddtypes';
+import List from '@mui/material/List';
+import ListItemText from '@mui/material/ListItemText';
+import Grid from '@mui/material/Grid';
+
+import Button from '@mui/material/Button';
+
+type RocaListProps = {
+  rocas: Roca[];
+  handleAddToCart(r:Roca): void;
+};
+//a
+function ShoppingCartComponent(rocas: RocaListProps): JSX.Element {
+
+
+
+    
+  return (
+    <>
+      <List id="catalogo">
+      {rocas.rocas.map((roca,index)=>{
+        return (
+          <div className="producto">
+
+            <Grid item xs={12}>
+              <img src={roca.img} style={{width:'100%'}} alt={roca.name.toString()}/>
+            </Grid>
+
+            <div className="infoProducto">
+
+                <ListItemText primary={roca.price+"€"} className="datoProducto"/>
+                <ListItemText primary={roca.type} className="datoProducto"/>
+                <ListItemText primary={roca.mohsHardness} className="datoProducto"/>
+                <ListItemText primary={roca.density} className="datoProducto"/>
+            </div>
+            <Grid item xs={12}>
+              <Button variant="contained" color="primary" style={{width:'100%'}} onClick={() => rocas.handleAddToCart(roca)} >Comprar</Button>
+            </Grid>
+
+          </div>
+        )
+      })}
+      </List>
+      
+    </>
+  );
+}
+
+export default ShoppingCartComponent;
