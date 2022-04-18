7ce20f65a331267f9ccdf8c5fe7e736585d79b24
Valen <uo277867@uniovi.es>
2022-04-18 23:11:57 +0200

    Api

--- webapp/src/api/api.ts
+++ webapp/src/api/api.ts
@@ -1,7 +1,8 @@
 import {User, Rock} from '../shared/shareddtypes';
 
+const apiEndPoint =process.env.REACT_APP_API_URI || 'http://localhost:5000/api';
+
 export async function addUser(user:User):Promise<boolean>{
-    const apiEndPoint= process.env.REACT_APP_API_URI || 'http://localhost:5000/api'
     let response = await fetch(apiEndPoint+'/users/add', {
         method: 'POST',
         headers: {'Content-Type':'application/json'},
@@ -14,14 +15,12 @@ export async function addUser(user:User):Promise<boolean>{
 }
 
 export async function getUsers():Promise<User[]>{
-    const apiEndPoint= process.env.REACT_APP_API_URI || 'http://localhost:5000/api'
     let response = await fetch(apiEndPoint+'/users/list');
     //The objects returned by the api are directly convertible to User objects
     return response.json()
 }
 
 export async function getRocas():Promise<Rock[]>{
-  const apiEndPoint= process.env.REACT_APP_API_URI || 'http://localhost:5000/api'
   let response = await fetch(apiEndPoint+'/rocks/list');
   //The objects returned by the api are directly convertible to User objects
   return response.json()
@@ -29,26 +28,26 @@ export async function getRocas():Promise<Rock[]>{
 
 
 export async function getRocksSedimentary():Promise<Rock[]>{
-  const apiEndPoint= process.env.REACT_APP_API_URI || 'http://localhost:5000/api'
   let response = await fetch(apiEndPoint+'/rocks/list/sedimentary');
   //The objects returned by the api are directly convertible to User objects
   return response.json()
 }
 export async function getRocksFiery():Promise<Rock[]>{
-  const apiEndPoint= process.env.REACT_APP_API_URI || 'http://localhost:5000/api'
   let response = await fetch(apiEndPoint+'/rocks/list/fiery');
   //The objects returned by the api are directly convertible to User objects
   return response.json()
 }
 export async function getRocksMetamorphic():Promise<Rock[]>{
-  const apiEndPoint= process.env.REACT_APP_API_URI || 'http://localhost:5000/api'
   let response = await fetch(apiEndPoint+'/rocks/list/metamorphic');
   //The objects returned by the api are directly convertible to User objects
   return response.json()
 }
-
+export async function getFilteredRocks(mohsMin:Number,mohsMax:Number,densityMin:Number,densityMax:Number,priceMin:Number,priceMax:Number,nameSubString:string):Promise<Rock[]> {
+  let response = await fetch(apiEndPoint+'/rocks/list/metamorphic');
+  //The objects returned by the api are directly convertible to User objects
+  return response.json()
+}
 export async function checkUser(email:String,password:String):Promise<boolean>{
-  const apiEndPoint= process.env.REACT_APP_API_URI || 'http://localhost:5000/api'
   let response = await fetch(apiEndPoint+'/users/login', {
       method: 'POST',
       headers: {'Content-Type':'application/json'},
@@ -63,7 +62,6 @@ export async function checkUser(email:String,password:String):Promise<boolean>{
 
 export async function getDeliveryCosts(address:String):Promise<Number>{
   console.log("BBBBBBBBBBBBBBBBBBBBBBBBB");
-  const apiEndPoint= process.env.REACT_APP_API_URI || 'http://localhost:5000/api'
   let response = await fetch(apiEndPoint+'/orders/deliveryCosts', {
       method: 'POST',
       headers: {'Content-Type':'application/json'},
