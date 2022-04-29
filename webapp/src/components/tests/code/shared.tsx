a8b33d177f8dcf941e8c1d81d8ac0cb881b09681
UO276900 <adrian.estrada2001@gmail.com>
2022-04-30 00:08:18 +0200

    Fichero shared para tests

---
+++ webapp/src/components/tests/code/shared.tsx
@@ -0,0 +1,12 @@
+import { Order } from "../../../shared/shareddtypes"
+
+export const LIST_OF_ORDERS_TEST : Order[]= [
+    {
+      name: "test0",
+      img: "test0img",
+      price: 0,
+      mohsHardness: 0,
+      density: 0,
+      type: "test0type",
+      quantityCart: 0,
+    },
--- webapp/src/components/tests/user/Orders.test.tsx
+++ webapp/src/components/tests/user/Orders.test.tsx
@@ -1,12 +1,14 @@
 import Orders from '../../Orders';
 import { render } from '@testing-library/react';
 import { BrowserRouter as Router } from "react-router-dom";
+import { LIST_OF_ORDERS_TEST } from "../code/shared";
 
 test('Check basic content of user Orders', async () => {
     
         const { container } = render(
         <Router> 
             <Orders email={"admin@gmail.com"}/>
+            testOrders={LIST_OF_ORDERS_TEST}
         </Router>
         )
         
