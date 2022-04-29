0d5165db8eec7ffdf0964df4d9643c323c0c6f50
UO276900 <adrian.estrada2001@gmail.com>
2022-04-29 23:55:04 +0200

    Test orders arreglado

--- webapp/src/components/tests/user/Orders.test.tsx
+++ webapp/src/components/tests/user/Orders.test.tsx
@@ -16,18 +16,3 @@ test('Check basic content of user Orders', async () => {
         expect(container).toHaveTextContent("Precio")    
         
 });
-
-test('Check user orders', async () => {
-    
-    const { container } = render(
-    <Router> 
-        <Orders email={"admin@gmail.com"}/>
-    </Router>
-    )
-    
-    expect(container).toHaveTextContent("Granito")
-    expect(container).toHaveTextContent("magmática")
-    expect(container).toHaveTextContent("2022")
-    expect(container).toHaveTextContent("Precio")    
-    
-});
