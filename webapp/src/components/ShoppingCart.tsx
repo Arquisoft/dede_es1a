eee22d34b134c5e5eae6faa06660e7c8259cda0e
valydumitru01 <62762857+valydumitru01@users.noreply.github.com>
2022-05-02 00:28:12 +0200

    Cambio estilo carrito + arreglo bug theme

--- webapp/src/components/ShoppingCart.tsx
+++ webapp/src/components/ShoppingCart.tsx
@@ -1,56 +1,64 @@
-import { Button, Grid } from '@mui/material';
-import { Rock } from '../shared/shareddtypes';
-import CartItem from './CartItem';
-import '../css/ShoppingCart.css';
-import LoginPod from './solid-pods/LoginPod';
-import {useNavigate} from 'react-router-dom';
+import { AppBar, Button, Grid, Typography } from "@mui/material";
+import { Rock } from "../shared/shareddtypes";
+import CartItem from "./CartItem";
+import "../css/ShoppingCart.css";
+import LoginPod from "./solid-pods/LoginPod";
+import { useNavigate } from "react-router-dom";
 
-
-type Props = {
-    cartContent: Rock[];
-    handleAddToCart: (selectedItem: Rock) => void;
-    handleRemoveFromCart: (id: string) => void;
+type CartProps = {
+  cartContent: Rock[];
+  handleAddToCart: (selectedItem: Rock) => void;
+  handleRemoveFromCart: (id: string) => void;
 };
 
-const Cart: React.FC<Props> = ({ cartContent, handleAddToCart, handleRemoveFromCart }) => {
-    const getTotalPrice = () => cartContent.reduce((sum: number, item) => sum + item.quantityCart * item.price, 0);
+function Cart(props: CartProps): JSX.Element {
+  const getTotalPrice = () =>
+    props.cartContent.reduce(
+      (sum: number, item) => sum + item.quantityCart * item.price,
+      0
+    );
 
-    return (
-        <Grid sx = {{width: 500}}>
-            <h1 id='title-cart'>Mi carrito</h1>
-            {cartContent.length === 0 ? <h3>El carrito está vacío</h3> : null}
-            
-            <div className="items-cart">
-                {cartContent.map(item => (
-                <CartItem 
-                    key={item.id} 
-                    item={item} 
-                    handleAddToCart={handleAddToCart}
-                    handleRemoveFromCart={handleRemoveFromCart}
-                />
-                ))}
-                <h2 id="price-cart">Total (iva 21% incluido): {getTotalPrice().toFixed(2)} €</h2>
-            </div>
+  return (
+    <>
+      {props.cartContent.length !== 0 ? (
+        <>
+          <Grid sx={{ width: 500 }}>
+            <AppBar color="secondary" position="static">
+              <Typography variant="h5">Mi Carrito</Typography>
+            </AppBar>
+            <Typography variant="h5">
+              Total (iva 21% incluido): {getTotalPrice().toFixed(2)} €
+            </Typography>
 
             <Button
-                size="medium"
-                disableElevation
-                variant="contained"
-                disabled={cartContent.length<=0}
-                onClick={() => { 
-                    if(sessionStorage.getItem("userLogged"))
-                        window.location.href = '/payment'; 
-                    else
-                        window.location.href = '/login'; 
-                }}
+              disableElevation
+              fullWidth
+              color="secondary"
+              variant="contained"
+              disabled={props.cartContent.length <= 0}
+              onClick={() => {
+                if (sessionStorage.getItem("userLogged"))
+                  window.location.href = "/payment";
+                else window.location.href = "/login";
+              }}
             >
-                Realizar Pedido
+              Realizar Pedido
             </Button>
-
-            
-
-        </Grid>
-    )
-};
+            {props.cartContent.map((item) => (
+              <CartItem
+                key={item.id}
+                item={item}
+                handleAddToCart={props.handleAddToCart}
+                handleRemoveFromCart={props.handleRemoveFromCart}
+              />
+            ))}
+          </Grid>
+        </>
+      ) : (
+        <Typography variant="h3">El carrito está vacío</Typography>
+      )}
+    </>
+  );
+}
 
 export default Cart;
