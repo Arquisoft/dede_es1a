e8b59487e5667f6c4aedb8d9eccf7e1f69362507
valydumitru01 <62762857+valydumitru01@users.noreply.github.com>
2022-05-01 21:29:58 +0200

    Finalizada vista producto

--- webapp/src/components/ProductView.tsx
+++ webapp/src/components/ProductView.tsx
@@ -2,8 +2,10 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getRocksById } from "../api/api";
 import "../css/LoginRegister.css";
-import { Card, CardMedia, Grid, Typography } from "@mui/material";
+import { Button, Card, CardMedia, Grid, Paper, Typography } from "@mui/material";
+import InfoIcon from '@mui/icons-material/Info';
 import { Rock } from "../shared/shareddtypes";
+import ProductViewData from "./ProductViewData";
 type RegisterProps = {
   handleAddToCart(r: Rock): void;
 };
@@ -21,55 +23,66 @@ function ProductView(props: RegisterProps): JSX.Element {
   if (rock !== undefined)
     return (
       <>
-      <Card>
+        <Card>
 
-    
-        <Grid container
-          style={{ width: '100%' }}
-          direction="column"
-          alignItems="center"
-          padding={2}>
-          <Grid item>
-            <Typography variant="h2">{rock.name}</Typography>
-          </Grid>
-          <CardMedia component="img"
-          height="500"
-          image={rock.img}
-          alt={rock.name}
-        />
-          <Grid item>
-            <Grid container spacing={2} >
-              <Grid item xs={6} >
-                <Typography textAlign={'center'}>Tipo</Typography>
-              </Grid>
-              <Grid item xs={6} >
-                <Typography textAlign={'center'}>{rock.type}</Typography>
-              </Grid>
 
-              <Grid item xs={6}>
-                <Typography textAlign={'center'}>Densidad</Typography>
-              </Grid>
-              <Grid item xs={6}>
-                <Typography textAlign={'center'}>{rock.density} g/cm3</Typography>
-              </Grid>
+          <Grid container
+            style={{ width: '100%' }}
+            direction="column"
+            alignItems="center"
+          >
+            <Grid item>
+              <Typography variant="h2">{rock.name}</Typography>
+            </Grid>
+            <CardMedia component="img"
+              height="500"
+              width="600"
+              image={rock.img}
+              alt={rock.name}
+            />
+            <Grid item>
+              <Grid container spacing={2} marginTop={2}>
+                <Grid item xs={6}>
+                <ProductViewData infoContent={"Hay 3 tipos de rocas dependiendo de su proveniencia: Sedimentarias, Ígneas y Metamórficas"} content={"Tipo"} />
 
-              <Grid item xs={6}>
-                <Typography textAlign={'center'}>Mohs</Typography>
-              </Grid>
-              <Grid item xs={6}>
-                <Typography textAlign={'center'}>{rock.mohsHardness}</Typography>
-              </Grid>
+                </Grid>
+                <Grid item xs={6}>
+                <ProductViewData  content={rock.type} />
+                </Grid>
+                <Grid item xs={6}>
+                <ProductViewData infoContent={"La medida internacional es g/m³"} content={"Densidad"} />
+                </Grid>
+                <Grid item xs={6}>
+                <ProductViewData  content={rock.density+" g/m³"} />
+                </Grid>
 
-              <Grid item xs={6}>
-                <Typography textAlign={'center'}>Precio</Typography>
-              </Grid>
-              <Grid item xs={6}>
-                <Typography textAlign={'center'}>{rock.price}€</Typography>
+                <Grid item xs={6}>
+                <ProductViewData infoContent={"La escala de Mohs es una relación de diez minerales ordenados por su dureza, de menor a mayor. Tiene una relacion exponencial relativa a las unidades de dureza (10 Mohs ≈ 7000 kg/mm²)"} content={"Mohs"} />
+                </Grid>
+                <Grid item xs={6}>
+                <ProductViewData content={rock.mohsHardness.toString()} />
+                </Grid>
+
+                <Grid item xs={6}>
+                <ProductViewData content={"Precio"} />
+                </Grid>
+                <Grid item xs={6}>
+                <ProductViewData content={rock.price+"€"}  />
+                </Grid>
+                <Button
+                  sx={{ marginBottom: '4em' }}
+                  variant="contained"
+                  className='btnBuy'
+                  color="primary"
+
+                  style={{ width: '100%' }}
+                  onClick={() => props.handleAddToCart(rock)}>
+                  Comprar</Button>
+
               </Grid>
             </Grid>
-          </Grid>
 
-        </Grid>
+          </Grid>
         </Card>
       </>
     );
