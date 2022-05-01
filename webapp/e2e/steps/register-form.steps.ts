4e3d6421365a5954fe80d39c3704a948d7dd3369
UO276900 <adrian.estrada2001@gmail.com>
2022-05-02 00:26:31 +0200

    Corregido Given, When, Then Test Register End2End

--- webapp/e2e/steps/register-form.steps.ts
+++ webapp/e2e/steps/register-form.steps.ts
@@ -1,252 +1,248 @@
 import { defineFeature, loadFeature } from 'jest-cucumber';
 import puppeteer from "puppeteer";
 
+const apiEndPoint= process.env.REACT_APP_URI|| 'http://localhost:3000'
+
 const feature = loadFeature('./features/register-form.feature');
 
 let page: puppeteer.Page;
+let page2: puppeteer.Page;
 let browser: puppeteer.Browser;
 
 defineFeature(feature, test => {
   
-  jest.setTimeout(100000)
-  beforeAll(async () => {
-
+  beforeEach(async () => {
     browser = process.env.GITHUB_ACTIONS
-    ? await puppeteer.launch()
-    : await puppeteer.launch({ headless: true, slowMo:100}); //false to run tests locally
-  page = await browser.newPage();
+      ? await puppeteer.launch()
+      : await puppeteer.launch({ headless: true });
+    page = await browser.newPage();
 
     await page
-      .goto("http://localhost:3000", {
+      .goto(apiEndPoint + "/register", {
         waitUntil: "networkidle0",
       })
       .catch(() => {});
+
+      page2 = await browser.newPage();
+
+      await page2
+        .goto(apiEndPoint + "/login", {
+         waitUntil: "networkidle0",
+        })
+        .catch(() => {}); 
   });
-  
-  test("User Register", ({given,when,then}) => {
-    let email:string
-    let name:string
-    let dni:string
-    let password:string
-    let confirmPassword:string
 
-    given("Email and password of a user", () => {
-      email = "adri@gmail.com"
-      name = "Adri"
-      dni = "12345678"
-      password = "adri"
-      confirmPassword = "adri"
+
+  test('The user is not registered in the site', ({given,when,then}) => {
+    
+    let name:string;
+    let surname:string;
+    let email:string;
+    let password:string;
+
+    given('An unregistered user', () => {
+      name = "efren"
+      surname = "garcia"
+      email = "efrengv15@gmail.com"
+      password = "error"
     });
 
-    when("I click in Regístrate", async () => {
-      await page.setViewport({ width: 1200, height: 1300 });
-      await expect(page).toMatch("Sedimentarias");
-      await expect(page).toClick("a[href='/register']");
-      await expect(page).toMatch("Crear cuenta");
-      await expect(page).toFill("input[name='email']", email);
-      await expect(page).toFill("input[name='name']", name);
-      await expect(page).toFill("input[name='dni']", dni);
-      await expect(page).toFill("input[name='password']", password);
-      await expect(page).toFill("input[name='confirmPassword']", confirmPassword);
-      await expect(page).toClick('button', { text: 'Regístrate' });
+    when('I fill the data in the form and press Regístrate', async () => {
+      await expect(page).toMatch('Crear cuenta')
+      
+      await expect(page).toFill('#name', name);
+      await expect(page).toFill('#surname', surname);
+      await expect(page).toFill('#email', email);
+      await expect(page).toFill('#pass', password);
+      await expect(page).toFill('#repPass', password);
+
+      await expect(page).toClick('button', { text: 'Crear cuenta' })
     });
 
-    then("I should be redirected to the login page", async () => {
-      await page.waitForNavigation()
-      await page.waitForTimeout(2000);
-      await expect(page).toMatch("Iniciar Sesión");
+    then('I should be redirected to Login Page', async () => {
+      await expect(page).toMatch('El e-mail ya existe')
     });
+  })
+
+
+  test('User Register blank email', ({given,when,then}) => {
+
+
+    given('Name, Dni, Password and confirmPassword of a user', () => {
+    });
+
+    when('I fill the data in the form and press Regístrate', async () => {
+      await expect(page).toMatch('Crear cuenta')
+      await expect(page).toClick('button', { text: 'Crear cuenta' })
+    });
+
+    then('Warning below email', async () => {
+      await expect(page).toMatch('La casilla no puede estar vacia')
+    });
+  })
+
+  test('User Register blank name', ({given,when,then}) => {
+    
+    let name:string;
+    let surname:string;
+    let email:string;
+    let password:string;
+
+    given('Email, Dni, Password and confirmPassword of a user', () => {
+      name = "efren"
+      surname = "garcia"
+      email = "efrengv15@gmail.com"
+      password = "error"
+    });
+
+    when('I fill the data in the form and press Regístrate', async () => {
+      await expect(page).toMatch('Crear cuenta')
+      
+      await expect(page).toFill('#name', name);
+      await expect(page).toFill('#surname', surname);
+      await expect(page).toFill('#email', email);
+      await expect(page).toFill('#pass', password);
+      await expect(page).toFill('#repPass', password);
+
+      await expect(page).toClick('button', { text: 'Crear cuenta' })
+    });
+
+    then('Warning below name', async () => {
+      await expect(page).toMatch('El e-mail ya existe')
+    });
+  })
+
+  test('User Register blank dni', ({given,when,then}) => {
+    
+    let name:string;
+    let surname:string;
+    let email:string;
+    let password:string;
+
+    given('Name, Email, Password and confirmPassword of a user', () => {
+      name = "efren"
+      surname = "garcia"
+      email = "efrengv15@gmail.com"
+      password = "error"
+    });
+
+    when('I fill the data in the form and press Regístrate', async () => {
+      await expect(page).toMatch('Crear cuenta')
+      
+      await expect(page).toFill('#name', name);
+      await expect(page).toFill('#surname', surname);
+      await expect(page).toFill('#email', email);
+      await expect(page).toFill('#pass', password);
+      await expect(page).toFill('#repPass', password);
+
+      await expect(page).toClick('button', { text: 'Crear cuenta' })
+    });
+
+    then('Warning below dni', async () => {
+      await expect(page).toMatch('El e-mail ya existe')
+    });
+  })
+
+  test('User Register blank password', ({given,when,then}) => {
+    
+    let name:string;
+    let surname:string;
+    let email:string;
+    let password:string;
+
+    given('Name, Dni, Email and confirmPassword of a user', () => {
+      name = "efren"
+      surname = "garcia"
+      email = "efrengv15@gmail.com"
+      password = "error"
+    });
+
+    when('I fill the data in the form and press Regístrate', async () => {
+      await expect(page).toMatch('Crear cuenta')
+      
+      await expect(page).toFill('#name', name);
+      await expect(page).toFill('#surname', surname);
+      await expect(page).toFill('#email', email);
+      await expect(page).toFill('#pass', password);
+      await expect(page).toFill('#repPass', password);
+
+      await expect(page).toClick('button', { text: 'Crear cuenta' })
+    });
+
+    then('Warning below Password', async () => {
+      await expect(page).toMatch('El e-mail ya existe')
+    });
+  })
+
+  test('User Register blank confirmPassword', ({given,when,then}) => {
+    
+    let name:string;
+    let surname:string;
+    let email:string;
+    let password:string;
+
+    given('Name, Dni, Password and Email of a user', () => {
+      name = "efren"
+      surname = "garcia"
+      email = "efrengv15@gmail.com"
+      password = "error"
+    });
+
+    when('I fill the data in the form and press Regístrate', async () => {
+      await expect(page).toMatch('Crear cuenta')
+      
+      await expect(page).toFill('#name', name);
+      await expect(page).toFill('#surname', surname);
+      await expect(page).toFill('#email', email);
+      await expect(page).toFill('#pass', password);
+      await expect(page).toFill('#repPass', password);
+
+      await expect(page).toClick('button', { text: 'Crear cuenta' })
+    });
+
+    then('Warning below confirmPassword', async () => {
+      await expect(page).toMatch('El e-mail ya existe')
+    });
+  })
+
+  test('Password and Confirm Password dont match', ({given,when,then}) => {
+    
+    let name:string;
+    let surname:string;
+    let email:string;
+    let password:string;
+
+    given('Email, Name, Dni, Password and confirmPassword of a user', () => {
+      name = "efren"
+      surname = "garcia"
+      email = "efrengv15@gmail.com"
+      password = "error"
+    });
+
+    when('I fill the data in the form and press Regístrate', async () => {
+      await expect(page).toMatch('Crear cuenta')
+      
+      await expect(page).toFill('#name', name);
+      await expect(page).toFill('#surname', surname);
+      await expect(page).toFill('#email', email);
+      await expect(page).toFill('#pass', password);
+      await expect(page).toFill('#repPass', password);
+
+      await expect(page).toClick('button', { text: 'Crear cuenta' })
+    });
+
+    then('Warning below confirmPassword (Las contraseñas no coinciden)', async () => {
+      await expect(page).toMatch('El e-mail ya existe')
+    });
+  })
+
+  afterEach(async ()=>{
+    browser.close()
   });
-  test("User Register blank email", ({given,when,then}) => {
-    let email:string
-    let name:string
-    let dni:string
-    let password:string
-    let confirmPassword:string
 
-    given("Name, Dni, Password and confirmPassword of a user", () => {
-      email = ""
-      name = "Adri"
-      dni = "12345678"
-      password = "adri"
-      confirmPassword = "adri"
-    });
 
-    when("I click in Regístrate", async () => {
-      await page.setViewport({ width: 1200, height: 1300 });
-      await expect(page).toMatch("Sedimentarias");
-      await expect(page).toClick("a[href='/register']");
-      await expect(page).toMatch("Crear cuenta");
-      await expect(page).toFill("input[name='email']", email);
-      await expect(page).toFill("input[name='name']", name);
-      await expect(page).toFill("input[name='dni']", dni);
-      await expect(page).toFill("input[name='password']", password);
-      await expect(page).toFill("input[name='confirmPassword']", confirmPassword);
-      await expect(page).toClick('button', { text: 'Regístrate' });
-    });
-
-    then("Warning below email", async () => {
-      await expect(page).toMatch("El campo no puede estar vacio");
-    });
+  afterEach(async ()=>{
+    browser.close()
   });
-  test("User Register blank name", ({given,when,then}) => {
-    let email:string
-    let name:string
-    let dni:string
-    let password:string
-    let confirmPassword:string
 
-    given("Email, Dni, Password and confirmPassword of a user", () => {
-      email = "adri@gmail.com"
-      name = ""
-      dni = "12345678"
-      password = "adri"
-      confirmPassword = "adri"
-    });
-
-    when("I click in Regístrate", async () => {
-      await page.setViewport({ width: 1200, height: 1300 });
-      await expect(page).toMatch("Sedimentarias");
-      await expect(page).toClick("a[href='/register']");
-      await expect(page).toMatch("Crear cuenta");
-      await expect(page).toFill("input[name='email']", email);
-      await expect(page).toFill("input[name='name']", name);
-      await expect(page).toFill("input[name='dni']", dni);
-      await expect(page).toFill("input[name='password']", password);
-      await expect(page).toFill("input[name='confirmPassword']", confirmPassword);
-      await expect(page).toClick('button', { text: 'Regístrate' });
-    });
-
-    then("Warning below name", async () => {
-      await expect(page).toMatch("El campo no puede estar vacio");
-    });
-  });
-  test("User Register blank dni", ({given,when,then}) => {
-    let email:string
-    let name:string
-    let dni:string
-    let password:string
-    let confirmPassword:string
-
-    given("Email, Name, Password and confirmPassword of a user", () => {
-      email = "adrian@gmail.com"
-      name = "Adri"
-      dni = ""
-      password = "adri"
-      confirmPassword = "adri"
-    });
-
-    when("I click in Regístrate", async () => {
-      await page.setViewport({ width: 1200, height: 1300 });
-      await expect(page).toMatch("Sedimentarias");
-      await expect(page).toClick("a[href='/register']");
-      await expect(page).toMatch("Crear cuenta");
-      await expect(page).toFill("input[name='email']", email);
-      await expect(page).toFill("input[name='name']", name);
-      await expect(page).toFill("input[name='dni']", dni);
-      await expect(page).toFill("input[name='password']", password);
-      await expect(page).toFill("input[name='confirmPassword']", confirmPassword);
-      await expect(page).toClick('button', { text: 'Regístrate' });
-    });
-
-    then("Warning below dni", async () => {
-      await expect(page).toMatch("El campo no puede estar vacio");
-    });
-  });
-  test("User Register blank password", ({given,when,then}) => {
-    let email:string
-    let name:string
-    let dni:string
-    let password:string
-    let confirmPassword:string
-
-    given("Email, Name, Dni and confirmPassword of a user", () => {
-      email = "adrian@gmail.com"
-      name = "Adri"
-      dni = "123456"
-      password = ""
-      confirmPassword = "adri"
-    });
-
-    when("I click in Regístrate", async () => {
-      await page.setViewport({ width: 1200, height: 1300 });
-      await expect(page).toMatch("Sedimentarias");
-      await expect(page).toClick("a[href='/register']");
-      await expect(page).toMatch("Crear cuenta");
-      await expect(page).toFill("input[name='email']", email);
-      await expect(page).toFill("input[name='name']", name);
-      await expect(page).toFill("input[name='dni']", dni);
-      await expect(page).toFill("input[name='password']", password);
-      await expect(page).toFill("input[name='confirmPassword']", confirmPassword);
-      await expect(page).toClick('button', { text: 'Regístrate' });
-    });
-
-    then("Warning below password", async () => {
-      await expect(page).toMatch("El campo no puede estar vacio");
-    });
-  });
-  test("User Register blank confirm password", ({given,when,then}) => {
-    let email:string
-    let name:string
-    let dni:string
-    let password:string
-    let confirmPassword:string
-
-    given("Email, Name, Dni Password of a user", () => {
-      email = "adrian@gmail.com"
-      name = "Adri"
-      dni = "123456"
-      password = "adri"
-      confirmPassword = ""
-    });
-
-    when("I click in Regístrate", async () => {
-      await page.setViewport({ width: 1200, height: 1300 });
-      await expect(page).toMatch("Sedimentarias");
-      await expect(page).toClick("a[href='/register']");
-      await expect(page).toMatch("Crear cuenta");
-      await expect(page).toFill("input[name='email']", email);
-      await expect(page).toFill("input[name='name']", name);
-      await expect(page).toFill("input[name='dni']", dni);
-      await expect(page).toFill("input[name='password']", password);
-      await expect(page).toFill("input[name='confirmPassword']", confirmPassword);
-      await expect(page).toClick('button', { text: 'Regístrate' });
-    });
-
-    then("Warning below confirmPassword", async () => {
-      await expect(page).toMatch("El campo no puede estar vacio");
-    });
-  });
-  test("Confirm Password and Password don't match", ({given,when,then}) => {
-    let email:string
-    let name:string
-    let dni:string
-    let password:string
-    let confirmPassword:string
-
-    given("Email, Name, Dni Password of a user", () => {
-      email = "adrian@gmail.com"
-      name = "Adri"
-      dni = "123456"
-      password = "adri"
-      confirmPassword = "manuel"
-    });
-
-    when("I click in Regístrate", async () => {
-      await page.setViewport({ width: 1200, height: 1300 });
-      await expect(page).toMatch("Sedimentarias");
-      await expect(page).toClick("a[href='/register']");
-      await expect(page).toMatch("Crear cuenta");
-      await expect(page).toFill("input[name='email']", email);
-      await expect(page).toFill("input[name='name']", name);
-      await expect(page).toFill("input[name='dni']", dni);
-      await expect(page).toFill("input[name='password']", password);
-      await expect(page).toFill("input[name='confirmPassword']", confirmPassword);
-      await expect(page).toClick('button', { text: 'Regístrate' });
-    });
-
-    then("Warning below confirmPassword", async () => {
-      await expect(page).toMatch("Las contraseñas no coinciden");
-    });
-  });
 });
