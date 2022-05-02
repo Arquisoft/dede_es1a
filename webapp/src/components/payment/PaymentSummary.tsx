da736b28d70e2853f507b5bf15334fc863642b29
UO276853 <UO276853@uniovi.es>
2022-05-02 19:04:39 +0200

    Cambiado componentes por Grid

--- webapp/src/components/payment/PaymentSummary.tsx
+++ webapp/src/components/payment/PaymentSummary.tsx
@@ -17,67 +17,84 @@ type Props = {
     simplificate: boolean; // true: solo muestra total  : false: muestra desglose (+ envio)
 };
 
-const PaymentSummary: React.FC<Props> = ({cartContent, simplificate}) => {
+const PaymentSummary: React.FC<Props> = ({ cartContent, simplificate }) => {
 
     const getTotalPrice = () => cartContent.reduce((sum: number, item) => sum + item.quantityCart * item.price, 0);
-    
+
     function getAddressContent() {
         const memoryCart = localStorage.getItem("address");
-        
-        if (memoryCart) 
+
+        if (memoryCart)
             return memoryCart;
-        else 
-          return "";
+        else
+            return "";
     }
 
-    const[addressOK, setAddresOk] = useState<boolean>();
+    const [addressOK, setAddresOk] = useState<boolean>();
     const [deliveryCosts, setDeliveryCosts] = useState<Number>();
     const findDC = async () => {
-        if(localStorage.getItem("address")){
+        if (localStorage.getItem("address")) {
             setDeliveryCosts(await getDeliveryCosts(getAddressContent()));
             setAddresOk(true);
-        }else{
+        } else {
             setAddresOk(false);
         }
     }
-    
+
     useEffect(() => {
         findDC();
     }, []);
-    
-    function getFinalDeliveryCosts(){
-        if (deliveryCosts){
+
+    function getFinalDeliveryCosts() {
+        if (deliveryCosts) {
             return (Number(deliveryCosts.toString()) + Number(getTotalPrice())).toFixed(2);
-        }else{
+        } else {
             return Number(getTotalPrice()).toFixed(2);
         }
     }
 
     return (
-        <div id='bill-payment'>
-            <Typography variant="h4" component="h4">Resumen de Pago</Typography>
-            <Typography variant="h4" component="h4">Costes (no iva): {  (getTotalPrice() - (getTotalPrice()*0.21)).toFixed(2) }€</Typography>
-            <Typography variant="h4" component="h4">Costes (iva 21%): { getTotalPrice().toFixed(2) }€</Typography>
+        <Grid container spacing={2} rowSpacing={2} >
 
-            {simplificate ? 
+            
+            <Grid item xs={12} ></Grid>
+            <Grid item xs={12} ></Grid>
+            <Grid item xs={12} >
+                <Typography variant="h4" component="h4">Resumen de Pago</Typography>
+            </Grid>
+
+            <Grid item xs={12} >
+                <Typography variant="h5" component="h5">Costes (no iva): {(getTotalPrice() - (getTotalPrice() * 0.21)).toFixed(2)}€</Typography>
+            </Grid>
+            <Grid item xs={12} >
+                <Typography variant="h5" component="h5">Costes (iva 21%): {getTotalPrice().toFixed(2)}€</Typography>
+            </Grid>
+
+
+            {simplificate ?
                 null
-                : 
-                <div>
+                :
+                <Grid item xs={12} >
                     {addressOK ?
-                        <Typography variant="h4" component="h4">
-                            Costes de Envio:{Number(deliveryCosts).toFixed(2)}€
+                        <Grid item xs={12} >
+                            <Typography variant="h5" component="h5">
+                                Costes de Envio:{Number(deliveryCosts).toFixed(2)}€
                             </Typography>
-                    :
-                        <Typography variant="h4" component="h4">
+                        </Grid>
+                        :
+                        <Grid item xs={12} ><Typography variant="h5" component="h5">
                             No se ha especificado una dirección correcta, el pedido sera almacenado en la tienda a espera de recogida
                         </Typography>
+                        </Grid>
+
                     }
                     
-                    <Typography variant="h4" component="h4"><h2>Total: {getFinalDeliveryCosts()}€</h2></Typography>
-                    
-                </div>
+                    <Grid item xs={12} >
+                        <Typography variant="h5" component="h5"><h2>Total: {getFinalDeliveryCosts()}€</h2></Typography>
+                    </Grid>
+                    </Grid>
             }
-        </div>
+        </Grid>
     )
 };
 
